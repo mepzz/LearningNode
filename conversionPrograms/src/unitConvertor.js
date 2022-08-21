63ea10e763f24cdddc917dda20e5a8415070c0d6
mepzz <111656909+mepzz@users.noreply.github.com>
2022-08-21 03:00:19 -0400

    Unit Conversion Program
    Wrote the function signature

---
+++ conversionPrograms/src/unitConvertor.js
@@ -0,0 +1,21 @@
+let weightKg = 87
+let heightCm = 174
+let convertedWeight = 5
+
+const weightLb = convertWeight();
+const heightFt = convertHeight();
+console.log(weightLb);
+console.log(heightFt);
+
+function convertWeight(){           //Funtion to convert kg to lbs.
+    
+    
+    return convertedWeight
+}
+
+function convertHeight(){          //Funtion to convert cm to ft.
+
+
+    
+    return[convertedHeight];
+}
