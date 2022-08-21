d3945c748c0bbe790de1f812f3210b6a53c611f7
mepzz <111656909+mepzz@users.noreply.github.com>
2022-08-21 03:08:09 -0400

    Functions complete
    Kg to Lb & Cm to Ft functions are roughly complete.

--- conversionPrograms/src/unitConvertor.js
+++ conversionPrograms/src/unitConvertor.js
@@ -1,20 +1,24 @@
 let weightKg = 87
 let heightCm = 174
-let convertedWeight = 5
+let convertedWeight = 0
+let convertedHeight = 0
+
+
 
 const weightLb = convertWeight();
 const heightFt = convertHeight();
 console.log(weightLb);
 console.log(heightFt);
 
-function convertWeight(){           //Funtion to convert kg to lbs.
+function convertWeight(){           //Funtion to convert kg to lbs. 1 KG = 2.2 lb
     
-    
+    convertedWeight = weightKg * 2.2
+
     return convertedWeight
 }
 
-function convertHeight(){          //Funtion to convert cm to ft.
-
+function convertHeight(){          //Funtion to convert cm to ft. 1 cm = 0.032808 ft
+    convertedHeight = heightCm * 0.032808 
 
     
     return[convertedHeight];
